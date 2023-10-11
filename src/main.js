2eae68f14eadaea219734d88e92541f7245d8325
Klaus Eckelt <Klaus.Eckelt@jku.at>
2023-10-11 10:23:02 +0200

    diff individual images

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import './style.css'
-import a from './A.png'
-import b from './B.png'
+import "./style.css";
+import a from "./A.png";
+import b from "./B.png";
 
 async function yolo() {
   // load a into canvas #a
-  const canvasA = document.getElementById('a');
+  const canvasA = document.getElementById("a");
   const imgA = new Image();
   imgA.src = a;
   await imgA.decode();
@@ -13,7 +13,7 @@ async function yolo() {
   canvasA.width = imgA.width;
   canvasA.height = imgA.height;
 
-  const ctx = canvasA.getContext('2d');
+  const ctx = canvasA.getContext("2d");
   if (ctx === null) {
     return;
   }
@@ -22,9 +22,8 @@ async function yolo() {
   // ctx.fillRect(0, 0, canvasA.width, canvasA.height);
   ctx.drawImage(imgA, 0, 0);
 
-
   // load a into canvas #a
-  const canvasB = document.getElementById('b');
+  const canvasB = document.getElementById("b");
   const imgB = new Image();
   imgB.src = b;
   await imgB.decode();
@@ -33,118 +32,96 @@ async function yolo() {
   canvasB.width = imgA.width;
   canvasB.height = imgA.height;
 
-  const ctxB = canvasB.getContext('2d');
+  const ctxB = canvasB.getContext("2d");
   if (ctxB === null) {
     return;
   }
 
   // fill with white first  in case it is smaller
-  ctxB.fillStyle = 'white';
+  ctxB.fillStyle = "white";
   ctxB.fillRect(0, 0, canvasB.width, canvasB.height);
   ctxB.drawImage(imgB, 0, 0);
 
   const baseImgData = ctx.getImageData(0, 0, canvasA.width, canvasA.height);
   const baseImg = cv.matFromImageData(baseImgData);
 
-  const compareImageData = ctxB.getImageData(0, 0, canvasB.width, canvasB.height);
+  const compareImageData = ctxB.getImageData(
+    0,
+    0,
+    canvasB.width,
+    canvasB.height
+  );
   const compareImg = cv.matFromImageData(compareImageData);
 
-  const orb = new cv.ORB();
-  const baseKeypoints = new cv.KeyPointVector();
-  const compareKeypoints = new cv.KeyPointVector();
-  const baseDescriptors = new cv.Mat();
-  const compareDescriptors = new cv.Mat();
-  orb.detectAndCompute(baseImg, new cv.Mat(), baseKeypoints, baseDescriptors);
-  orb.detectAndCompute(compareImg, new cv.Mat(), compareKeypoints, compareDescriptors);
-
-  const bfMatcher = new cv.BFMatcher(cv.NORM_HAMMING, true);
-  const matches = new cv.DMatchVector();
-  bfMatcher.match(baseDescriptors, compareDescriptors, matches);
-  let matchScore = 0;
-  for (let i = 0; i < matches.size(); i++) {
-    matchScore += matches.get(i).distance;
-  }
-  matchScore /= matches.size();
-  const descriptorBits = 32 * 8; // see https://docs.opencv.org/4.8.0/db/d95/classcv_1_1ORB.html#ac166094ca013f59bfe3df3b86fa40bfe
-  matchScore /= descriptorBits; // normalize
-  console.log('matchScore', matchScore);
-  // show score in #score
-  const score = document.getElementById('score');
-  if (score !== null) {
-    // format to 3 floating points
-    score.innerText = matchScore.toFixed(3);
-  }
-
-  let diffImg = new cv.Mat();
-  cv.absdiff(compareImg, baseImg, diffImg);
-  const grayImg = new cv.Mat();
-  cv.cvtColor(diffImg, grayImg, cv.COLOR_BGR2GRAY);
-
-  const th = 1;
-  const imask = new cv.Mat();
-  cv.threshold(grayImg, imask, th, 255, cv.THRESH_BINARY);
-  cv.imshow('mask', imask);
-
-  const kernel = cv.Mat.ones(3, 3, cv.CV_8U);
-  const dilate = new cv.Mat();
-  // get iterations from slider #dilateIterations
-  let slider = document.getElementById('dilateIterations');
-  let iterations = slider != null ? parseInt((slider).value) : 2;
-
-  cv.dilate(imask, dilate, kernel, new cv.Point(-1, -1), iterations, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
-  cv.imshow('dilate', dilate);
-
-  const erode = new cv.Mat();
-  slider = document.getElementById('erodeIterations');
-  iterations = slider != null ? parseInt((slider).value) : 2;
-  cv.erode(dilate, erode, kernel, new cv.Point(-1, -1), iterations, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
-  cv.imshow('erode', erode);
-
-
-  const contours = new cv.MatVector();
-  const hierarchy = new cv.Mat();
-  cv.findContours(erode, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
-
+  orbScore(baseImg, compareImg);
+  
+  const removedContours = getDiffContours(compareImg, baseImg);
+  const addedContours = getDiffContours(baseImg, compareImg);
+  
   try {
-    const contoursArray = [];
-    for (let i = 0; i < contours.size(); i++) {
-      contoursArray.push(contours.get(i));
-    }
+    const contourAreaInput = document.getElementById("contourArea");
+    const maxArea = contourAreaInput != null ? parseInt(contourAreaInput.value) : Infinity;
 
-    slider = document.getElementById('transparency');
-    const transparency = slider != null ? parseFloat((slider).value) : 0;
-
-    let diffImg = new cv.Mat();
-    let dtype = -1;
-    cv.addWeighted(compareImg, 0.5-transparency, baseImg, 0.5+transparency, 0, diffImg, dtype);
-
-    slider = document.getElementById('contourArea');
-    const maxArea = slider != null ? parseInt((slider).value) : Infinity;
-
-    for (const contour of contoursArray) {
+    const thickness = 2;
+    const opacity = 255;
+    // draw added contours on compareImage
+    for (const contour of addedContours) {
       if (cv.contourArea(contour) < maxArea) {
         const rectangle = false;
-        if(rectangle) {
+        let color = new cv.Scalar(102, 194, 165, opacity);
+        if (rectangle) {
           const rect = cv.boundingRect(contour);
           const pt1 = new cv.Point(rect.x, rect.y);
           const pt2 = new cv.Point(rect.x + rect.width, rect.y + rect.height);
-          cv.rectangle(diffImg, pt1, pt2, new cv.Scalar(255, 0, 0, 255), 1); // scaler = color in RGB-Opacity format
+          cv.rectangle(compareImg, pt1, pt2,  color, thickness); // scaler = color in RGB-Opacity format
         } else {
           let hull = new cv.Mat();
           cv.convexHull(contour, hull, false, true);
-  
+          
           // Draw the convex hull
-          let color = new cv.Scalar(255, 0, 0, 255);
-          let thickness = 2;
           let lineType = cv.LINE_8;
           const hulls = new cv.MatVector();
-          hulls.push_back(hull);	
-          cv.drawContours(diffImg, hulls, 0, color, thickness, lineType);
+          hulls.push_back(hull);
+
+          // let overlay = new cv.Mat(compareImg.rows, compareImg.cols, compareImg.type(), new cv.Scalar(0, 0, 0, 255));
+          cv.drawContours(compareImg, hulls, 0, color, thickness, lineType);
+          // cv.addWeighted(overlay, 1, compareImg, 1, 0, compareImg, -1);
         }
       }
     }
 
-    cv.imshow('diff', diffImg);
+    // draw removed contours on base Image
+    for (const contour of removedContours) {
+      if (cv.contourArea(contour) < maxArea) {
+        const rectangle = false;
+        let color = new cv.Scalar(240, 82, 104, opacity);
+        if (rectangle) {
+          const rect = cv.boundingRect(contour);
+          const pt1 = new cv.Point(rect.x, rect.y);
+          const pt2 = new cv.Point(rect.x + rect.width, rect.y + rect.height);
+          cv.rectangle(baseImg, pt1, pt2, color, thickness,); // scaler = color in RGB-Opacity format
+        } else {
+          let hull = new cv.Mat();
+          cv.convexHull(contour, hull, false, true);
+
+          // Draw the convex hull
+          let lineType = cv.LINE_8;
+          const hulls = new cv.MatVector();
+          hulls.push_back(hull);
+          cv.drawContours(baseImg, hulls, 0, color, thickness, lineType);
+        }
+      }
+    }
+
+    // merge the images
+    const transparencySlider = document.getElementById("transparency");
+    const transparency = transparencySlider != null ? parseFloat(transparencySlider.value) : 0;
+
+    let diffImg = new cv.Mat();
+    let dtype = -1;
+    cv.addWeighted( compareImg, 0.5 + transparency, baseImg, 0.5 - transparency, 0, diffImg, dtype );
+
+    cv.imshow("diff", diffImg);
   } catch (e) {
     console.error(e);
   }
@@ -153,8 +130,6 @@ async function yolo() {
   // compareImg.copyTo(canvas, imask); // copy the pixels from img to canvas where mask is non-zero
   // cv.imshow('diff', canvas);
 
-
-
   // let diffImg = new cv.Mat();
   // let mask = new cv.Mat();
   // let dtype = -1;
@@ -162,16 +137,12 @@ async function yolo() {
   // cv.imshow('diff', diffImg);
   // mask.delete();
 
-
   // let diffImg = new cv.Mat();
   // let mask = new cv.Mat();
   // let dtype = -1;
   // cv.addWeighted(compareImg, 0.5, baseImg, 0.5, 0, diffImg, dtype);
   // cv.imshow('diff', diffImg);
   // mask.delete();
-
-
-  console.log('moin');
 
   // count how many array elements are unequal zero in diffimg
   // let diffCount = 0;
@@ -185,45 +156,38 @@ async function yolo() {
 
   baseImg.delete();
   compareImg.delete();
-  baseKeypoints.delete();
-  compareKeypoints.delete();
-  baseDescriptors.delete();
-  compareDescriptors.delete();
-  matches.delete();
   // diffImg.delete();
-};
-
+}
 
 if (cv.getBuildInformation) {
   // console.log(cv.getBuildInformation());
   yolo();
-}
-else {
+} else {
   // WASM
-  console.log('WASM');
-  cv['onRuntimeInitialized'] = () => {
+  console.log("WASM");
+  cv["onRuntimeInitialized"] = () => {
     // console.log(cv.getBuildInformation());
     yolo();
-  }
+  };
 }
 
-const syncCheckbox = document.getElementById('syncIterations');
-const dilateInput = document.getElementById('dilateIterations');
-const erodeInput = document.getElementById('erodeIterations');
+const syncCheckbox = document.getElementById("syncIterations");
+const dilateInput = document.getElementById("dilateIterations");
+const erodeInput = document.getElementById("erodeIterations");
 
-syncCheckbox.addEventListener('change', () => {
+syncCheckbox.addEventListener("change", () => {
   if (syncCheckbox.checked) {
     erodeInput.value = dilateInput.value;
   }
 });
 
-dilateInput.addEventListener('input', () => {
+dilateInput.addEventListener("input", () => {
   if (syncCheckbox.checked) {
     erodeInput.value = dilateInput.value;
   }
 });
 
-erodeInput.addEventListener('input', () => {
+erodeInput.addEventListener("input", () => {
   if (syncCheckbox.checked) {
     dilateInput.value = erodeInput.value;
   }
@@ -232,13 +196,13 @@ erodeInput.addEventListener('input', () => {
 let timeoutId;
 
 [
-  document.getElementById('dilateIterations'),
-  document.getElementById('erodeIterations'),
-  document.getElementById('contourArea'),
-  document.getElementById('transparency'),
+  document.getElementById("dilateIterations"),
+  document.getElementById("erodeIterations"),
+  document.getElementById("contourArea"),
+  document.getElementById("transparency"),
 ].forEach((slider) => {
   if (slider !== null) {
-    slider.addEventListener('input', () => {
+    slider.addEventListener("input", () => {
       if (timeoutId !== undefined) {
         clearTimeout(timeoutId);
       }
@@ -249,3 +213,79 @@ let timeoutId;
     });
   }
 });
+function getDiffContours(compareImg, baseImg) {
+  let diffImg = new cv.Mat();
+  // cv.absdiff(compareImg, baseImg, diffImg);
+  cv.subtract(compareImg, baseImg, diffImg);
+  const grayImg = new cv.Mat();
+  cv.cvtColor(diffImg, grayImg, cv.COLOR_BGR2GRAY);
+
+  const th = 1;
+  const imask = new cv.Mat();
+  cv.threshold(grayImg, imask, th, 255, cv.THRESH_BINARY);
+  cv.imshow("mask", imask);
+
+  const kernel = cv.Mat.ones(3, 3, cv.CV_8U);
+  const dilate = new cv.Mat();
+  // get iterations from slider #dilateIterations
+  let slider = document.getElementById("dilateIterations");
+  let iterations = slider != null ? parseInt(slider.value) : 2;
+
+  cv.dilate(imask, dilate, kernel, new cv.Point(-1, -1), iterations, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
+  cv.imshow("dilate", dilate);
+
+  const erode = new cv.Mat();
+  slider = document.getElementById("erodeIterations");
+  iterations = slider != null ? parseInt(slider.value) : 2;
+  cv.erode(dilate, erode, kernel, new cv.Point(-1, -1), iterations, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
+  cv.imshow("erode", erode);
+
+  const contours = new cv.MatVector();
+  const hierarchy = new cv.Mat();
+  cv.findContours(erode, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+  const contoursArray = [];
+  for (let i = 0; i < contours.size(); i++) {
+    contoursArray.push(contours.get(i));
+  }
+  return contoursArray
+}
+
+function orbScore(baseImg, compareImg) {
+  const orb = new cv.ORB();
+  const baseKeypoints = new cv.KeyPointVector();
+  const compareKeypoints = new cv.KeyPointVector();
+  const baseDescriptors = new cv.Mat();
+  const compareDescriptors = new cv.Mat();
+  orb.detectAndCompute(baseImg, new cv.Mat(), baseKeypoints, baseDescriptors);
+  orb.detectAndCompute(
+    compareImg,
+    new cv.Mat(),
+    compareKeypoints,
+    compareDescriptors
+  );
+
+  const bfMatcher = new cv.BFMatcher(cv.NORM_HAMMING, true);
+  const matches = new cv.DMatchVector();
+  bfMatcher.match(baseDescriptors, compareDescriptors, matches);
+  let matchScore = 0;
+  for (let i = 0; i < matches.size(); i++) {
+    matchScore += matches.get(i).distance;
+  }
+  matchScore /= matches.size();
+  const descriptorBits = 32 * 8; // see https://docs.opencv.org/4.8.0/db/d95/classcv_1_1ORB.html#ac166094ca013f59bfe3df3b86fa40bfe
+  matchScore /= descriptorBits; // normalize
+  console.log("matchScore", matchScore);
+  // show score in #score
+  const score = document.getElementById("score");
+  if (score !== null) {
+    // format to 3 floating points
+    score.innerText = matchScore.toFixed(3);
+  }
+  
+  baseKeypoints.delete();
+  compareKeypoints.delete();
+  baseDescriptors.delete();
+  compareDescriptors.delete();
+  matches.delete();
+}
+
